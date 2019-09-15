29279a2aa0478331ff9ea83786df36c9452d4f26
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2019-09-15 07:46:48 -0700

    feat: Adding FirstNeuralNetwork content

--- 2-Theano/theano_examples/theano4.py
+++ 2-Theano/theano_examples/theano4.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import theano
 from theano import tensor as T
-tensor3 =T.Tensortype(broadcastable=(False, False, False),dtype='float32')
-x =tensor3()
+#tensor3 =T.Tensortype(broadcastable=(False, False, False),dtype='float32')
+#x =tensor3()
+dtype='float32'
+
+ndim=1
+broadcast = (False,) * ndim
+name=None
+
+x = T.TensorType(dtype, broadcast)(name)
